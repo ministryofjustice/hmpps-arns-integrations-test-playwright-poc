12ca2cdb5e0e571655778ebed0756cc5faca1eb3
JY0280 <jean-yves@hippodigital.co.uk>
2025-03-07 10:51:02 +0000

    Added further tests on About page

--- tests/08.userViewsAboutPageCompleteSan.spec.ts
+++ tests/08.userViewsAboutPageCompleteSan.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 import { StubHomePage } from '../page-objects/stub-home-page';
 import { SentencePlanPage } from '../page-objects/sentence-plan-pages';
+import { Accessibility } from '../page-objects/accessibility';
 
 /* Note: this test feature will fail if the test data is wiped. 
 It relies on pre-existing PK with a completed SAN assessment. */
@@ -8,6 +9,7 @@ test('User views about page when they have completed SAN assessment', async ({ p
 
   const stubHomePage = new StubHomePage(page);
   const sentencePlanPage = new SentencePlanPage(page);
+  const accessibility = new Accessibility(page);
 
   // Navigate to the stub home page
   await stubHomePage.goto();
@@ -37,4 +39,19 @@ test('User views about page when they have completed SAN assessment', async ({ p
   // Check no banner displays
   await sentencePlanPage.checkBannerDoesntDisplayForCompleteAssessment();
   console.log('About page complete assessment - info verified');
+
+  // Check page has no accessiblity violations
+  await accessibility.shouldHaveNoAccessibilityViolations();
+
+  // Click show all sections accordion and check there are still no accessibility violations
+  await sentencePlanPage.clickShowAllSectionsAccordion();
+  await accessibility.shouldHaveNoAccessibilityViolations();
+
+  // Click create a goal from section
+  await sentencePlanPage.clickCreateAGoalLinkFromWithinSection();
+  await sentencePlanPage.checkCreateAGoalPageTitle();
+
+  // Back out from page and ensure user is back on About page
+  await sentencePlanPage.clickBackButtonFromCreateGoalPage();
+  await sentencePlanPage.checkAboutPageTitle();
 });

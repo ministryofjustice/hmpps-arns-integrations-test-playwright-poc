9caa742e7d696d09f0fd119d6830da3ae6ac7eb8
devkokov <dm.kokov@gmail.com>
2025-04-16 12:26:17 +0100

    ACE-22 Add test logging

--- tests/00.databaseConnectivity.spec.ts
+++ tests/00.databaseConnectivity.spec.ts
@@ -2,7 +2,8 @@ import { test, expect } from '@playwright/test';
 import { runQuery } from '../utils/oracle';
 
 test('verify Oracle DB connectivity', async () => {
-  const result = await runQuery(`SELECT 1 FROM dual`);
+  const result = await runQuery(`SELECT 'd[-_-]b' AS test FROM dual`);
   expect(result.rows).toBeDefined();
   expect(result.rows.length).toBe(1);
+  console.log('Oracle DB connection successful.', result.rows)
 });

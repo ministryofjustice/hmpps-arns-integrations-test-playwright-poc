b347ea00c606c0c768e916fa2cb347014cd0483a
JY0280 <jean-yves@hippodigital.co.uk>
2025-05-09 11:08:34 +0100

    Added simulation for thinking time, updated values for VUs to more realistic ones

--- performance/tests/browserAndApi.js
+++ performance/tests/browserAndApi.js
@@ -19,7 +19,7 @@ export const options = {
       scenarios.browser = {
         executor: 'constant-vus',
         exec: 'browserTest',
-        vus: 1,
+        vus: 3,
         duration: '2m',
         // adjust the above values as needed if you need to run browser an API scenarios together 
         options: {
@@ -36,9 +36,9 @@ export const options = {
         executor: 'ramping-vus',
         exec: 'runIfLoad',
         stages: [
-          { duration: '2m', target: 500 },   // Ramp up to 500 VUs over 2 minutes
-          { duration: '5m', target: 2000 },  // Ramp up to 2000 VUs over 5 minutes
-          { duration: '5m', target: 2000 },  // Hold 2000 VUs for 5 minutes
+          { duration: '2m', target: 100 },   // Ramp up to 500 VUs over 2 minutes
+          { duration: '5m', target: 400 },  // Ramp up to 2000 VUs over 5 minutes
+          { duration: '5m', target: 400 },  // Hold 2000 VUs for 5 minutes
           { duration: '1m', target: 0 },     // Ramp down over 1 minute
         ],
         gracefulRampDown: '1m',
@@ -59,8 +59,8 @@ export const options = {
         executor: 'shared-iterations',
         exec: 'runIfSmoke',
         vus: 1,
-        iterations: 1,
-        maxDuration: '30s',
+        iterations: 30,
+        maxDuration: '15s',
       };
     }
     if (SCENARIOS.includes('stress')) {
@@ -96,9 +96,9 @@ export async function browserTest() {
     // Access SAN assessment
     await page.locator('//*[@id="target-service"]').selectOption('strengths-and-needs-assessment');
     await page.locator('//*[@id="form"]/div[1]/div/div/button[1]').click();
-    sleep(1);
+    sleep(1 + Math.random() * 3); // simulate thinking time
     await page.locator('//*[@id="main-content"]/div[3]/div/div/div[2]/a').click();
-    sleep(2);
+    sleep(1 + Math.random() * 3);
 
     // Move driver
     const pages = browser.context().pages();
@@ -111,73 +111,77 @@ export async function browserTest() {
 
     // Select No accomodation
     await page2.locator('#current_accommodation-3').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
 
     // Select emergency hostel
     await page2.locator('#type_of_no_accommodation-2').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
 
     // Submit form
     await page2.locator('//*[@id="form"]/div[2]/button').click();
-    sleep(3);
+    sleep(1 + Math.random() * 3);
 
     // Submit no accomodation form
     await page2.locator('//*[@id="no_accommodation_reason-6"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="suitable_housing_planned-3"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="accommodation_changes-4"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="form"]/div[5]/button').click();
 
     // Submit practitioner analysis
     await page2.locator('//*[@id="tab_practitioner-analysis"]').click();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="accommodation_practitioner_analysis_strengths_or_protective_factors-2"]').check();
     await page2.locator('//*[@id="accommodation_practitioner_analysis_risk_of_serious_harm-2"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="accommodation_practitioner_analysis_risk_of_reoffending"]').check();
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="accommodation_practitioner_analysis_risk_of_reoffending_yes_details"]').type('Performance test');
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="form"]/div[4]/button').click();
     sleep(3);
 
     // Move onto Employment and education section
     await page2.locator('//*[@id="main-content"]/div/div[3]/div[1]/nav/ul/li[2]/a/span').click();
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="employment_status-2"]').check();
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="form"]/div[2]/button').click();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
 
     await page2.locator('//*[@id="employment_other_responsibilities"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="employment_history"]').click();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="education_highest_level_completed-8"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="education_professional_or_vocational_qualifications-4"]').check();
     await page2.locator('//*[@id="education_transferable_skills-2"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="education_difficulties-5"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="employment_experience"]').check();
     await page2.locator('//*[@id="education_experience"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="employment_education_changes-3"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
 
     // Submit form
     await page2.locator('//*[@id="form"]/div[11]/button').click();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
 
     // Submit practitioner analysis
     await page2.locator('//*[@id="tab_practitioner-analysis"]').click();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="employment_education_practitioner_analysis_strengths_or_protective_factors-2"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="employment_education_practitioner_analysis_risk_of_serious_harm-2"]').check();
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="employment_education_practitioner_analysis_risk_of_reoffending"]').check();
     await page2.locator('//*[@id="employment_education_practitioner_analysis_risk_of_reoffending_yes_details"]').type('Performance test');
-    sleep(1);
+    sleep(1 + Math.random() * 3);
     await page2.locator('//*[@id="form"]/div[4]/button').click();
     sleep(3);
 
@@ -192,7 +196,7 @@ export function apiTest() {
 
   // Send the GET request and retrieve cookies
   const res = http.get(testStubUrl);
-  sleep(1);
+  sleep(Math.random() * 3) // comment out when running stress scenario
 
   // Check that the response status is 200
   check(res, {
@@ -216,7 +220,8 @@ export function apiTest() {
   check(postRes, {
     'status is 200': (r) => r.status === 200,
   });
-  sleep(1);
+  //console.log(postRes).toString;
+  sleep(Math.random() * 3) // comment out when running stress scenario
 }
 
 export function runIfLoad() {

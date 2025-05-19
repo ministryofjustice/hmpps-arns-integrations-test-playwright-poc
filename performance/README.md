9b0042d8ec041583b2f4cb3d0730feb670744961
JY0280 <78161148+JY0280@users.noreply.github.com>
2025-05-19 14:31:13 +0100

    Ace 95 implement performance tests (#31)
    * early ideas
    
    * Started adding a browser test feature for performance with performance evaluation markers
    
    * removed previous research
    
    * Play with K6 experimental browser
    
    * Experimenting browser and api requests in one script
    
    * added more steps to the browser test
    
    * added ramp up stages to api scenario
    
    * Added thresholds to scenarios options
    
    * Extended browser test journey
    
    * added regions
    
    * Extended browser test
    
    * Finished employment and education pathway journey in browser test
    
    * Added README, and soak scenario. Added SCENARIO env var to control the API scenarios
    
    * Updated README
    
    * Updated README
    
    * Added data json to use in POST request
    
    * Fixed data import
    
    * added browser to SCENARIO environment variable to drive more combinations. Updated README
    
    * Added realistic stress scenario
    
    * Updated values in load test to mnore realistic values
    
    * added more sleeps to aim for more realistic behaviour in browser and to avoid artificial spikes that don't reflect actual usage.
    
    * Added simulation for thinking time, updated values for VUs to more realistic ones
    
    * Updated stress test to more gradual ram up, edited README
    
    * Added uuid generator to use in handover link navigation api request
    
    * Edited README. Added extract scenario to tests, added a run_test.sh file to run all commands in a single place
    
    * Updated performance tests README
    
    * Updated the extract command to not keep source=-console
    
    * Edited README
    
    * Edited README
    
    * Edited README
    
    * Edited values for load and stress tests
    
    * commenting out logging of extracted url variable value
    
    * Fixed run_test.sh
    
    * Edited README and bash script
    
    * Tidied up, added comments to README and run bash script
    
    * Minor edit to README
    
    * Final edits to bash run file and test feature options
    
    * Corrected typo in README
    
    * Addressing code review comments
    
    * More changes on the back of review
    
    * Fixing backticks attempt
    
    * #
    
    ---------
    
    Co-authored-by: JY0280 <jean-yves@hippodigital.co.uk>

---
+++ performance/README.md
@@ -0,0 +1,61 @@
+## Setup
+To run locally first install [k6](https://github.com/grafana/k6). The browser module is now included in the default install.
+`brew install k6`
+
+To run the tests head/headless you will need chromium installed - to install chromium run
+`brew install --force chromium --no-quarantine ` 
+
+## Test Configuration
+Tests are designed to allow running browser and API tests independently or in parallel. You can monitor the UI while applying different API load scenarios.
+
+Set the SCENARIO environment variable to control what test(s) are executed.
+
+Available Scenarios:
+
+BROWSER – Browser-based UI test
+
+API – Performance/load test scenarios:
+
+- load
+- soak
+- smoke
+- stress
+
+## Running the tests
+To run the tests locally these can be executed from the command line in a terminal. 
+From the root of the project, you can run the following commands, or navigate to the directory of the run_test.sh file and run that (you can configure which scenario to run in there too).
+
+- Browser test only
+`SCENARIO=browser k6 run performance/tests/browserAndApi.js`
+
+- API tests
+Before running those, you need to capture the one-time handover link as  extracted url. The extract scenario must be run first to get that value, which is run as part of the execution of the bash script.
+
+## Technical description:
+
+The run_test.sh script is a Bash automation wrapper designed to run a two-phase performance testing workflow using k6. It first executes a browser-based extraction step to retrieve a dynamic URL, then passes that URL as an environment variable to run following API-based performance tests.
+
+To use the run_test.sh file to run the api tests, navigate to the `utils` directory and run:
+
+```
+chmod +x run_test.sh
+./run_test.sh
+```
+
+To run the browser tests in head mode and see the browser (HEADLESS:FALSE)
+`SCENARIO=browser K6_BROWSER_HEADLESS=false k6 run performance/tests/browserAndApi.js`
+
+NOTE: you may want to adjust the `duration` value in the Browser test to match which API scenario you're running, but not necessarily.
+
+
+## The report
+
+At the end of the test a report is generated with an array of different metrics that have been captured during the tests. 
+There have been thresholds set on a selection of metrics that have been specified in the load model 
+* Server response time is lower than 200 milliseconds in 90% of requests and lower than
+500 milliseconds in 100% of requests
+This is captured in `browser_http_req_duration` && `browser_http_req_failed`
+* The API test metrics are also monitored 
+This is captured in `http_req_duration` && `http_req_failed`
+
+Should any of these exceed the selected threshold metric an error will be displayed in the test results.
---
+++ performance/tests/browserAndApi.js
@@ -0,0 +1,277 @@
+import { browser } from 'k6/browser';
+import { check } from 'k6';
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+const testStubUrl = 'https://arns-oastub-test.hmpps.service.justice.gov.uk/';
+const TARGET_URL = __ENV.TARGET_URL || testStubUrl;
+
+function simulateThinkingTime() {
+  sleep(1 + Math.random() * 3);
+}
+
+
+const SCENARIOS = (__ENV.SCENARIO || 'smoke').split(',').map(s => s.trim());
+// splitting scenarios into a list to set which api test to run alongside the browser test
+
+//#region options
+export const options = {
+  scenarios: (function () {
+    const scenarios = {};
+
+    if (SCENARIOS.includes('extract')) {
+      scenarios.extract = {
+        executor: 'shared-iterations',
+        exec: 'extractUrl',
+        vus: 1,
+        iterations: 1,
+        maxDuration: '30s',
+        options: {
+          browser: {
+            type: 'chromium',
+            headless: true,
+          },
+        },
+      };
+    }
+
+    if (SCENARIOS.includes('browser')) {
+      scenarios.browser = {
+        executor: 'constant-vus',
+        exec: 'browserTest',
+        vus: 3,
+        duration: '5m',
+        // adjust the above value as needed if you need to run browser an API scenarios together 
+        options: {
+          browser: {
+            type: 'chromium',
+            headless: false,
+          },
+        },
+      };
+    }
+
+    if (SCENARIOS.includes('load')) {
+      scenarios.api = {
+        executor: 'ramping-vus',
+        exec: 'runIfLoad',
+        // High load
+        stages: [
+          { duration: '2m', target: 1000 },  // Ramp up to 1k VUs in 2 minutes
+          { duration: '3m', target: 2000 },  // Ramp up to 2k VUs in 3 minutes
+          { duration: '2m', target: 3000 },  // Ramp to 3k VUs in 2 minutes
+          { duration: '5m', target: 3000 },  // Hold at 3k VUs for 5 minutes
+          { duration: '2m', target: 0 },     // Ramp down to 0
+        ],
+        // Lower load
+        /*stages: [
+         { duration: '1m', target: 50 },   // ramp to 50 users
+         { duration: '2m', target: 150 },  // ramp to 150 users
+         { duration: '2m', target: 300 },  // ramp to 300 users
+         { duration: '2m', target: 300 },  // hold at 300
+         { duration: '1m', target: 0 },    // ramp down
+       ],*/
+        gracefulRampDown: '1m',
+      };
+    }
+
+    if (SCENARIOS.includes('soak')) {
+      scenarios.api = {
+        executor: 'constant-vus',
+        exec: 'runIfSoak',
+        vus: 50,
+        duration: '1h',
+      };
+    }
+
+    if (SCENARIOS.includes('smoke')) {
+      scenarios.api = {
+        executor: 'shared-iterations',
+        exec: 'runIfSmoke',
+        vus: 1,
+        iterations: 1,
+        maxDuration: '15s',
+      };
+    }
+    if (SCENARIOS.includes('stress')) {
+      scenarios.api = {
+        executor: 'ramping-vus',
+        exec: 'runIfStress',
+        startVUs: 0,
+        stages: [
+          { duration: '1m', target: 1000 },  // ramp to 1k users
+          { duration: '1m', target: 2000 },  // ramp to 2k users
+          { duration: '1m', target: 3000 },  // ramp to 3k users
+          { duration: '2m', target: 3500 },  // push beyond expected peak
+          { duration: '2m', target: 3500 },  // hold to observe system under stress
+          { duration: '1m', target: 0 },     // ramp down
+        ],
+        gracefulRampDown: '30s',
+      };
+    }
+    return scenarios;
+  })(),
+
+  thresholds: {
+    browser_http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    browser_http_req_duration: ['p(90)<200', 'p(95)<500'], // 90% of requests should be below 200ms / 95% of requests should be below 500ms
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<200'], // 95% of requests should be below 200ms
+  },
+};
+//#endregion
+
+//#region browser test
+
+export async function browserTest() {
+  const stubPage = await browser.newPage();
+
+  try {
+    await stubPage.goto(TARGET_URL, { waitUntil: 'networkidle' });
+
+    // Access SAN assessment
+    await stubPage.locator('//*[@id="target-service"]').selectOption('strengths-and-needs-assessment');
+    await stubPage.locator('//*[@id="form"]/div[1]/div/div/button[1]').click();
+    simulateThinkingTime(); // simulate thinking time
+    await stubPage.locator('//*[@id="main-content"]/div[3]/div/div/div[2]/a').click();
+    simulateThinkingTime();
+
+    // Move driver
+    const pages = browser.context().pages();
+    const sanPage = pages[pages.length - 1];
+
+    await sanPage.bringToFront();
+    console.log('Current URL after switching: ', sanPage.url())
+    console.log(sanPage.title());
+    console.log("=========================");
+
+    // Select No accomodation
+    await sanPage.locator('#current_accommodation-3').check();
+    simulateThinkingTime();
+
+    // Select emergency hostel
+    await sanPage.locator('#type_of_no_accommodation-2').check();
+    simulateThinkingTime();
+
+    // Submit form
+    await sanPage.locator('//*[@id="form"]/div[2]/button').click();
+    simulateThinkingTime();
+
+    // Submit no accomodation form
+    await sanPage.locator('//*[@id="no_accommodation_reason-6"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="suitable_housing_planned-3"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="accommodation_changes-4"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="form"]/div[5]/button').click();
+
+    // Submit practitioner analysis
+    await sanPage.locator('//*[@id="tab_practitioner-analysis"]').click();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="accommodation_practitioner_analysis_strengths_or_protective_factors-2"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="accommodation_practitioner_analysis_risk_of_serious_harm-2"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="accommodation_practitioner_analysis_risk_of_reoffending"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="accommodation_practitioner_analysis_risk_of_reoffending_yes_details"]').type('Performance test');
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="form"]/div[4]/button').click();
+    sleep(3);
+
+    // Move onto Employment and education section
+    await sanPage.locator('//*[@id="main-content"]/div/div[3]/div[1]/nav/ul/li[2]/a/span').click();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_status-2"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="form"]/div[2]/button').click();
+    simulateThinkingTime();
+
+    await sanPage.locator('//*[@id="employment_other_responsibilities"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_history"]').click();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="education_highest_level_completed-8"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="education_professional_or_vocational_qualifications-4"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="education_transferable_skills-2"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="education_difficulties-5"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_experience"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="education_experience"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_education_changes-3"]').check();
+    simulateThinkingTime();
+
+    // Submit form
+    await page2.locator('//*[@id="form"]/div[11]/button').click();
+    simulateThinkingTime();
+
+    // Submit practitioner analysis
+    await sanPage.locator('//*[@id="tab_practitioner-analysis"]').click();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_education_practitioner_analysis_strengths_or_protective_factors-2"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_education_practitioner_analysis_risk_of_serious_harm-2"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_education_practitioner_analysis_risk_of_reoffending"]').check();
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="employment_education_practitioner_analysis_risk_of_reoffending_yes_details"]').type('Performance test');
+    simulateThinkingTime();
+    await sanPage.locator('//*[@id="form"]/div[4]/button').click();
+    sleep(3);
+
+  } finally {
+    await page.close();
+  }
+}
+//#endregion
+
+//#region extract handover url
+
+export async function extractUrl() {
+  const page = await browser.newPage();
+  try {
+    await page.goto(TARGET_URL, { waitUntil: 'networkidle' });
+
+    await page.locator('//*[@id="target-service"]').selectOption('strengths-and-needs-assessment');
+    await page.locator('//*[@id="form"]/div[1]/div/div/button[1]').click();
+    sleep(1);
+
+    // Get one-time link
+    const element = await page.locator('//*[@id="one-time-link"]');
+    await element.waitFor();
+
+    // Get the value
+    const extractedUrl = await element.getAttribute('value');
+    console.log(`EXTRACTED_URL=${extractedUrl}`);
+
+  } finally {
+    await page.close();
+  }
+}
+
+//#endregion
+
+//#region api test
+export function apiTest() {
+
+  const extractedUrl = __ENV.EXTRACTED_URL;
+  const res = http.get(extractedUrl);
+
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+  });
+  sleep(1); // can comment out when running stress scenario
+}
+
+export function runIfLoad() { apiTest(); }
+export function runIfSoak() { apiTest(); }
+export function runIfSmoke() { apiTest(); }
+export function runIfStress() { apiTest(); }
+
+//#endregion
